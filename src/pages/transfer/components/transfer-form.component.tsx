5dc64319b5eccc9aafbdbcfb22a473b70b5ea703
Miwue11 <hr2miguellopez@gmail.com>
2025-05-30 13:21:31 +0200

    seguimos

--- src/pages/transfer/components/transfer-form.component.tsx
+++ src/pages/transfer/components/transfer-form.component.tsx
@@ -25,11 +25,44 @@ export const TransferFormComponent: React.FC<Props> = (props) => {
           <select>
             {accountList.map((account) => (
               <option key={account.id} value={account.id}>
-                {account.alias} - {account.iban}
+                {account.alias}
               </option>
             ))}
           </select>
         </div>
+        <div>
+          <label>Ingrese el IBAN de la cuenta destino:</label>
+          <input />
+        </div>
+        <div>
+          <label>Beneficiario:</label>
+          <input />
+        </div>
+        <div>
+          <label>Importe</label>
+          <input type="number" />
+        </div>
+        <div>
+          <label>Concepto:</label>
+          <input type="concept" />
+        </div>
+        <div>
+          <label>Observaciones:</label>
+          <input type="notes" />
+        </div>
+        <div>
+          <p>
+            Para que la transferencia se realice en otra fecha diferente a la de
+            hoy, por favor, indiquenos la fecha de ejecución:
+            <label>Fecha de ejecución</label>
+            <input name="realDaleTransfer" type="date" />
+          </p>
+        </div>
+        <div>
+          <p>Escriba una direccion email para dar aviso al beenficiario:</p>
+          <input type="email" />
+        </div>
+        <button type="submit">REALIZAR LA TRANSFERENCIA</button>
       </form>
     </div>
   );
